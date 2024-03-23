04c7d5c6d0a898120acf6b1f882289f7c87da596
Alfie7719 <149108010+Alfie7719@users.noreply.github.com>
2024-03-23 22:43:23 +0000

    Add files via upload

---
+++ ServerSide/Functions.js
@@ -0,0 +1,19 @@
+function clearEditor() {
+    editor.setValue('');
+  }
+  
+  function openFile() {
+    // You can implement file opening functionality here
+    console.log('Open file functionality');
+  }
+  
+  function saveFile() {
+    // You can implement file saving functionality here
+    console.log('Save file functionality');
+  }
+  
+  function saveAsFile() {
+    // You can implement file saving as functionality here
+    console.log('Save as file functionality');
+  }
+  
